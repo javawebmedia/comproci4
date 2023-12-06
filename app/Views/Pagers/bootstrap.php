3575c25121d88261674bf1ffb4e9a3b6e184d771
Java Web Media <javawebmedia@gmail.com>
2023-12-06 12:01:06 +0700

    Update pagers

--- app/Views/Pagers/bootstrap.php
+++ app/Views/Pagers/bootstrap.php
@@ -3,12 +3,12 @@
     <ul class="pagination">
         <?php if ($pager->hasPrevious()) { ?>
             <li class="page-item">
-                 <a href="<?= $pager->getFirst() ?>" aria-label="First" class="page-link">
+                 <a href="<?= $pager->getFirst() ?>" aria-label="First" class="page-link pb-2 pt-2">
                     <span aria-hidden="true">First</span>
                 </a>
             </li>
             <li class="page-item">
-                <a href="<?= $pager->getPrevious() ?>" class="page-link" aria-label="Previous">
+                <a href="<?= $pager->getPrevious() ?>" class="page-link pb-2 pt-2" aria-label="Previous">
                     <span>&laquo;</span>
                 </a>
             </li>
@@ -19,7 +19,7 @@
                 $activeclass = $link['active']?'active':'';
         ?>
             <li class="page-item <?= $activeclass ?>">
-                <a href="<?= $link['uri'] ?>" class="page-link">
+                <a href="<?= $link['uri'] ?>" class="page-link pb-2 pt-2">
                     <?= $link['title'] ?>
                 </a>
             </li>
@@ -27,12 +27,12 @@
 
         <?php if ($pager->hasNext()) { ?>
             <li class="page-item">
-                <a href="<?= $pager->getNext() ?>" aria-label="Next" class="page-link">
+                <a href="<?= $pager->getNext() ?>" aria-label="Next" class="page-link pb-2 pt-2">
                     <span aria-hidden="true">&raquo;</span>
                 </a>
             </li>
             <li class="page-item">
-                 <a href="<?= $pager->getLast() ?>" aria-label="Last" class="page-link">
+                 <a href="<?= $pager->getLast() ?>" aria-label="Last" class="page-link pb-2 pt-2">
                     <span aria-hidden="true">Last</span>
                 </a>
             </li>
