71d53e725ef3eeb8723f7660d680be33c2375eea
Java Web Media <javawebmedia@gmail.com>
2023-12-06 12:04:40 +0700

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'root',
-        'password'     => 'root',
+        'password'     => '',
         'database'     => 'marketplace_website',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
